c807f9a188289de332a8f8d6789697bd81e8604c
Rander Wang <rander.wang@linux.intel.com>
2019-05-31 18:24:19 +0800

    ASoC: SOF: Fix PM suspend order for codec and controller
    Implement similar runtime PM idle logic as the Intel
    AZX HDA driver. If any HDA codecs are powered when runtime
    suspend request comes, return EBUSY. By doing this, we
    enforce strict ordering between HDA codec and the controller
    when performing suspend.
    
    Signed-off-by: Rander Wang <rander.wang@linux.intel.com>
    Signed-off-by: Kai Vehmanen <kai.vehmanen@linux.intel.com>

--- sound/soc/sof/intel/hda-dsp.c
+++ sound/soc/sof/intel/hda-dsp.c
@@ -458,6 +458,19 @@ int hda_dsp_runtime_suspend(struct snd_sof_dev *sdev, int state)
 	return hda_suspend(sdev, state, true);
 }
 
+int hda_dsp_runtime_idle(struct snd_sof_dev *sdev)
+{
+	struct hdac_bus *hbus = sof_to_bus(sdev);
+
+	if (hbus->codec_powered) {
+		dev_dbg(sdev->dev, "codecs still powered (%lX), not idle\n",
+			hbus->codec_powered);
+		return -EBUSY;
+	}
+
+	return 0;
+}
+
 int hda_dsp_suspend(struct snd_sof_dev *sdev, int state)
 {
 	struct hdac_bus *bus = sof_to_bus(sdev);
